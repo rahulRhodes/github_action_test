4c8c92135768574597daa24e1e34264abff4fc52
Rahul Singh <rahulsingh@Rahuls-MacBook-Air.local>
2025-10-24 15:09:28 +0530

    unit test renamed

---
+++ tests/test_math_operations.py
@@ -0,0 +1,14 @@
+from src.math_operations import add,sub
+
+
+
+def test_add():
+    assert add(2,3)==5
+    assert add(-1,1)==0
+    
+
+def test_sub():
+    assert sub(5,3)==2
+    assert sub(3,2)==1
+    assert sub(8,6)==2
+    
